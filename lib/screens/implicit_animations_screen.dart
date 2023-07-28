0c1e16c5910774bf5c9c403fde56c6652514eff6
kimdaeyeub <kdy3915@naver.com>
2023-07-28 14:39:57 +0900

    AnimatedContainer

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -30,20 +30,18 @@ class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedAlign(
-              alignment: _visible ? Alignment.topLeft : Alignment.topRight,
+            AnimatedContainer(
               duration: const Duration(
-                milliseconds: 300,
+                milliseconds: 500,
               ),
-              child: AnimatedOpacity(
-                duration: const Duration(
-                  milliseconds: 300,
-                ),
-                opacity: _visible ? 1 : 0,
-                child: Container(
-                  width: size.width * 0.8,
-                  height: size.height * 0.5,
-                  color: Colors.amber,
+              width: size.width * 0.8,
+              height: size.width * 0.8,
+              transform: Matrix4.rotationZ(_visible ? 1 : 0),
+              transformAlignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: _visible ? Colors.indigo : Colors.amber,
+                borderRadius: BorderRadius.circular(
+                  _visible ? 300 : 0,
                 ),
               ),
             ),
