5a105ece06f9ef8ca7be493549b3ca347225a877
kimdaeyeub <kdy3915@naver.com>
2023-07-29 22:42:35 +0900

    Random()

--- lib/screens/apple_watch_screen.dart
+++ lib/screens/apple_watch_screen.dart
@@ -16,12 +16,32 @@ class _AppleWatchScreenState extends State<AppleWatchScreen>
     duration: const Duration(
       seconds: 2,
     ),
-    lowerBound: 0.005,
-    upperBound: 2.0,
+  )..forward();
+
+  late final CurvedAnimation _curve = CurvedAnimation(
+    parent: _animationController,
+    curve: Curves.bounceOut,
   );
 
+  late Animation<double> _progress = Tween<double>(
+    begin: 0.005,
+    end: 1.5,
+  ).animate(_curve);
+
   void _animateValues() {
-    _animationController.forward();
+    final newBegin = _progress.value;
+    final random = Random();
+    final newEnd = random.nextDouble() * 2.0;
+    setState(() {
+      _progress = Tween(begin: newBegin, end: newEnd).animate(_curve);
+    });
+    _animationController.forward(from: 0);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -37,11 +57,11 @@ class _AppleWatchScreenState extends State<AppleWatchScreen>
       ),
       body: Center(
         child: AnimatedBuilder(
-          animation: _animationController,
+          animation: _curve,
           builder: (context, child) {
             return CustomPaint(
               painter: AppleWatchPainter(
-                progress: _animationController.value,
+                progress: _progress.value,
               ),
               size: const Size(400, 400),
             );
