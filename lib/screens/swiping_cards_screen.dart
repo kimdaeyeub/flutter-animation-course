12ca213198178db0af5dbecf13139fb69e7685fd
kimdaeyeub <kdy3915@naver.com>
2023-07-30 16:11:25 +0900

    Tween Transform

--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SwipingCardScreen extends StatefulWidget {
@@ -10,7 +12,7 @@ class SwipingCardScreen extends StatefulWidget {
 class _SwipingCardScreenState extends State<SwipingCardScreen>
     with SingleTickerProviderStateMixin {
   late final size = MediaQuery.of(context).size;
-  late final AnimationController _animationController = AnimationController(
+  late final AnimationController _position = AnimationController(
     vsync: this,
     duration: const Duration(milliseconds: 300),
     lowerBound: size.width * -1,
@@ -19,12 +21,17 @@ class _SwipingCardScreenState extends State<SwipingCardScreen>
   );
   double posX = 0;
 
+  late final Tween<double> _rotation = Tween(
+    begin: -15,
+    end: 15,
+  );
+
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    _animationController.value += details.delta.dx;
+    _position.value += details.delta.dx;
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
-    _animationController.animateTo(
+    _position.animateTo(
       0,
       curve: Curves.bounceOut,
     );
@@ -32,7 +39,7 @@ class _SwipingCardScreenState extends State<SwipingCardScreen>
 
   @override
   void dispose() {
-    _animationController.dispose();
+    _position.dispose();
     super.dispose();
   }
 
@@ -45,8 +52,12 @@ class _SwipingCardScreenState extends State<SwipingCardScreen>
         ),
       ),
       body: AnimatedBuilder(
-        animation: _animationController,
+        animation: _position,
         builder: (context, child) {
+          final angle = _rotation
+                  .transform((_position.value + size.width / 2) / size.width) *
+              pi /
+              180;
           return Stack(
             children: [
               Align(
@@ -55,13 +66,16 @@ class _SwipingCardScreenState extends State<SwipingCardScreen>
                   onHorizontalDragUpdate: _onHorizontalDragUpdate,
                   onHorizontalDragEnd: _onHorizontalDragEnd,
                   child: Transform.translate(
-                    offset: Offset(_animationController.value, 0),
-                    child: Material(
-                      elevation: 10,
-                      color: Colors.red.shade100,
-                      child: SizedBox(
-                        width: size.width * 0.8,
-                        height: size.height * 0.5,
+                    offset: Offset(_position.value, 0),
+                    child: Transform.rotate(
+                      angle: angle,
+                      child: Material(
+                        elevation: 10,
+                        color: Colors.red.shade100,
+                        child: SizedBox(
+                          width: size.width * 0.8,
+                          height: size.height * 0.5,
+                        ),
                       ),
                     ),
                   ),
