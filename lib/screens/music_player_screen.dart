4d749b510dbc7551fb0a468b8cfbe8341a5b9033
kimdaeyeub <kdy3915@naver.com>
2023-07-31 14:42:56 +0900

    Hero

--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_animation_masterclass/screens/music_player_detail_screen.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
   const MusicPlayerScreen({super.key});
@@ -22,6 +23,17 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
     setState(() {
       _currentPage = newPage;
     });
+  }
+
+  void _onTap(int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MusicPlayerDetailScreen(
+          index: index,
+        ),
+      ),
+    );
   }
 
   @override
@@ -84,25 +96,31 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                     builder: (context, value, child) {
                       final difference = (value - index).abs();
                       final scale = 1 - (difference * 0.2);
-                      return Transform.scale(
-                        scale: scale,
-                        child: Container(
-                          height: 350,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.4),
-                                blurRadius: 10,
-                                spreadRadius: 2,
-                                offset: const Offset(0, 8),
+                      return GestureDetector(
+                        onTap: () => _onTap(index + 1),
+                        child: Hero(
+                          tag: "${index + 1}",
+                          child: Transform.scale(
+                            scale: scale,
+                            child: Container(
+                              height: 350,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.4),
+                                    blurRadius: 10,
+                                    spreadRadius: 2,
+                                    offset: const Offset(0, 8),
+                                  ),
+                                ],
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    "assets/covers/${index + 1}.jpg",
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
                               ),
-                            ],
-                            image: DecorationImage(
-                              image: AssetImage(
-                                "assets/covers/${index + 1}.jpg",
-                              ),
-                              fit: BoxFit.cover,
                             ),
                           ),
                         ),
