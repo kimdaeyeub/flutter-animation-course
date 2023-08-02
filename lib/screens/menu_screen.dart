7c50d1286fdc54924252c3a89599832406b6bd48
kimdaeyeub <kdy3915@naver.com>
2023-08-02 20:52:48 +0900

    Importing Animation

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_animation_masterclass/screens/apple_watch_screen.dart';
 import 'package:flutter_animation_masterclass/screens/explicit_animations_screen.dart';
 import 'package:flutter_animation_masterclass/screens/implicit_animations_screen.dart';
 import 'package:flutter_animation_masterclass/screens/music_player_screen.dart';
+import 'package:flutter_animation_masterclass/screens/rive_screen.dart';
 import 'package:flutter_animation_masterclass/screens/swiping_cards_screen.dart';
 
 class MenuScreen extends StatelessWidget {
@@ -71,6 +72,15 @@ class MenuScreen extends StatelessWidget {
               ),
               child: const Text(
                 "Music Player",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => _goToPage(
+                context,
+                const RiveScreen(),
+              ),
+              child: const Text(
+                "Rive",
               ),
             ),
           ],
