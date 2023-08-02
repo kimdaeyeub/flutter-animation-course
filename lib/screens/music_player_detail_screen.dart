25f7b5ffbc7fa0d9c95d79829a131860c152257d
kimdaeyeub <kdy3915@naver.com>
2023-08-02 10:22:20 +0900

    VolumePainter

--- lib/screens/music_player_detail_screen.dart
+++ lib/screens/music_player_detail_screen.dart
@@ -54,14 +54,41 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
     super.dispose();
   }
 
+  bool _dragging = false;
+
+  void _toggleDragging() {
+    setState(() {
+      _dragging = !_dragging;
+    });
+  }
+
+  late final size = MediaQuery.of(context).size;
+
+  final ValueNotifier<double> _volume = ValueNotifier(0);
+
+  void _onVolumeDragUpdate(DragUpdateDetails details) {
+    _volume.value += details.delta.dx;
+
+    _volume.value = _volume.value.clamp(0.0, size.width - 80);
+  }
+
+  void _openMenu() {}
+
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Interstellar",
         ),
+        actions: [
+          IconButton(
+            onPressed: _openMenu,
+            icon: const Icon(
+              Icons.menu,
+            ),
+          ),
+        ],
       ),
       body: Column(
         children: [
@@ -186,9 +213,73 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
               ],
             ),
           ),
+          const SizedBox(
+            height: 30,
+          ),
+          GestureDetector(
+            onHorizontalDragUpdate: _onVolumeDragUpdate,
+            onHorizontalDragStart: (_) => _toggleDragging(),
+            onHorizontalDragEnd: (_) => _toggleDragging(),
+            child: AnimatedScale(
+              scale: _dragging ? 1.1 : 1.0,
+              duration: const Duration(
+                milliseconds: 500,
+              ),
+              curve: Curves.bounceOut,
+              child: Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ValueListenableBuilder(
+                  valueListenable: _volume,
+                  builder: (context, value, child) {
+                    return CustomPaint(
+                      size: Size(
+                        size.width - 80,
+                        50,
+                      ),
+                      painter: VolumePainter(
+                        volume: value,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
+  }
+}
+
+class VolumePainter extends CustomPainter {
+  final double volume;
+
+  VolumePainter({
+    required this.volume,
+  });
+  @override
+  void paint(Canvas canvas, Size size) {
+    final progress = volume.clamp(0.0, size.width);
+
+    final bgPaint = Paint()..color = Colors.grey.shade300;
+
+    final bgRect = Rect.fromLTWH(0, 0, size.width, size.height);
+
+    canvas.drawRect(bgRect, bgPaint);
+
+    final volumePaint = Paint()..color = Colors.grey.shade500;
+
+    final volumeRect = Rect.fromLTWH(0, 0, progress, size.height);
+
+    canvas.drawRect(volumeRect, volumePaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant VolumePainter oldDelegate) {
+    return oldDelegate.volume != volume;
   }
 }
 
